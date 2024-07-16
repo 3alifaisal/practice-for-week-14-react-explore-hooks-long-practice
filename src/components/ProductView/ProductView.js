827f3b2292c785474f0e528c26bf42709dfbcf79
3alifaisal <alifaisalawada000@gmail.com>
2024-07-16 23:46:21 +0200

    useEffect and useState practice
    Done very insightful learned a lot about state changes, about  the usage of local storage even. about hooks in general as well

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,35 +1,74 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css'
+import "./ProductView.css";
 
 function ProductView({ products }) {
-
+  console.log("Product View renders");
   // TODO: Replace with state variable
-  const sideOpen = true;
+  const [sideOpen, setSideOpen] = useState(() => {
+    const savedValue = localStorage.getItem("sideOpen");
+    return savedValue ? JSON.parse(savedValue) : false;
+  });
 
+  const [selectedProduct, setSelectedProduct] = useState(() => {
+    const savedValue = localStorage.getItem("selectedProduct");
+    return savedValue ? JSON.parse(savedValue) : "";
+  });
+  const isFirstRender = useRef(true);
+  useEffect(() => {
+    // Handing first render I thought this is cool
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    // Alternating so when we press the button it switchs the panel back to closed
+    if (selectedProduct !== "") {
+      setSideOpen(true);
+    } else {
+      setSideOpen(false);
+    }
+    localStorage.setItem("selectedProduct", JSON.stringify(selectedProduct));
+  }, [selectedProduct]);
+  useEffect(() => {
+    if (!sideOpen) {
+      setSelectedProduct("");
+    }
+    localStorage.setItem("sideOpen", JSON.stringify(sideOpen));
+  }, [sideOpen]);
   return (
     <div className="product-view">
       <div className="product-main-area">
         <h1>Products</h1>
         <div className="product-list">
-          {products.map(item =>
+          {products.map((item) => (
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              onClick={() =>
+                setSelectedProduct((prev) => {
+                  if (prev === item) {
+                    return "";
+                  }
+                  return item;
+                })
+              }
+              isSelected={selectedProduct.id === item.id ? true : false}
             />
-          )}
+          ))}
         </div>
       </div>
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
-          <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
-            {sideOpen ? '>' : '<'}
+          <div
+            className="product-side-panel-toggle"
+            onClick={() => setSideOpen((prev) => !prev)}
+          >
+            {sideOpen ? ">" : "<"}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
